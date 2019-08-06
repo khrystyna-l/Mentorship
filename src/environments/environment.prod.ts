56d4db6be9104b73a73e6096348ba247e82ea997
Khrystyna Lukaniuk <obnimysh@gmail.com>
2019-08-06 13:35:54 +0300

    adding firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig = {
+    apiKey: "AIzaSyBbNjfVB52crxizKVfyJpEXRBY8lCwVPp0",
+    authDomain: "fcc-book-trading-394e0.firebaseapp.com",
+    databaseURL: "https://fcc-book-trading-394e0.firebaseio.com",
+    projectId: "fcc-book-trading-394e0",
+    storageBucket: "",
+    messagingSenderId: "557225311113",
+    appId: "1:557225311113:web:00d105329fa82fc2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig = {
+    apiKey: "AIzaSyBbNjfVB52crxizKVfyJpEXRBY8lCwVPp0",
+    authDomain: "fcc-book-trading-394e0.firebaseapp.com",
+    databaseURL: "https://fcc-book-trading-394e0.firebaseio.com",
+    projectId: "fcc-book-trading-394e0",
+    storageBucket: "",
+    messagingSenderId: "557225311113",
+    appId: "1:557225311113:web:00d105329fa82fc2"
+  }
 };
 
 /*
