d85b7bd52a9ef4584079c5f1464ce4bcb92c976e
Khrystyna Lukaniuk <obnimysh@gmail.com>
2019-08-06 16:56:51 +0300

    configure firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// 2. Add your credentials from step 1
+const config = {
+  apiKey: 'AIzaSyBbNjfVB52crxizKVfyJpEXRBY8lCwVPp0',
+  authDomain: 'fcc-book-trading-394e0.firebaseapp.com',
+  databaseURL: 'https://fcc-book-trading-394e0.firebaseio.com',
+  projectId: 'fcc-book-trading-394e0',
+  storageBucket: '',
+  messagingSenderId: '557225311113',
+  appId: '1:557225311113:web:00d105329fa82fc2'
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // 3. Initialize
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
